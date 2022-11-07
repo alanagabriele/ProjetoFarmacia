2cc9f031a569658fc7f8ec6063891a7e434f6a48
Alana Gabriele <alanagabriele43@gmail.com>
2022-11-07 18:16:08 -0300

    Update ConnectionFactory.java
    alterações

--- src/br/com/farmacia/factory/ConnectionFactory.java
+++ src/br/com/farmacia/factory/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 	
 	private static final String url = "jdbc:mysql://localhost:3306/farmacia"; 
 	private static final String user = "root";
-	private static final String pass = "@05AlanA19";
+	private static final String pass = "";
 	
 	public static Connection createConnectionToMysql() {
 			
